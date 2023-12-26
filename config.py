3af48526b9e5911cd0dc0c39c57cb86736f652e2
KimGladen <ceniixster@gmail.com>
2023-12-26 20:09:45 +0800

    Remove unwanted files

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 
 import os
 
+
 class Config:
-    API_KEY = os.getenv("API_KEY", "yMHuTv2ARJ2E4/8V6OQJWw==")
-    DATABASE_NAME = 'public_transport6.db'
+    MONGO_CONNECTION_URL = os.getenv("MONGO_CONNECTION_URL", "mongodb://localhost:27017")
+    LTA_API_KEY = os.getenv("LTA_API_KEY", "yMHuTv2ARJ2E4/8V6OQJWw==")
