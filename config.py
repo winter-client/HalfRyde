76d656fb927b32b96a5c24d7384ccbae57510f69
KimGladen <ceniixster@gmail.com>
2023-12-26 20:15:47 +0800

    Remove config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-import os
-
-
-class Config:
-    MONGO_CONNECTION_URL = os.getenv("MONGO_CONNECTION_URL", "mongodb://localhost:27017")
-    LTA_API_KEY = os.getenv("LTA_API_KEY", "yMHuTv2ARJ2E4/8V6OQJWw==")
